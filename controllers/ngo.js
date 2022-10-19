a75c498d2b172442935ab6983a9440ec2f90a288
Rishav045 <82647816+Rishav045@users.noreply.github.com>
2022-10-19 15:14:34 +0530

    adding findOne function

--- controllers/ngo.js
+++ controllers/ngo.js
@@ -50,7 +50,7 @@ export const getNgos = async (req, res, next) => {
 }
 export const getNgoReg = async(req,res,next)=>{
     try{
-        const ngo1 = await Ngo.find({Unique_Registration_ID : req.params.reg});
+        const ngo1 = await Ngo.findOne({Unique_Registration_ID : req.params.reg});
         res.status(200).json(ngo1);
     }catch(err){
         next(err);
