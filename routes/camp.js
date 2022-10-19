5c2f7fca0d35a63f42f5761c3118019f152e938b
Soumya Ranjan Sahu <88780525+SaM-0777@users.noreply.github.com>
2022-10-19 21:06:30 +0530

    Update camp.js

--- routes/camp.js
+++ routes/camp.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createCamp, deleteCamp, getCamp, getCamps, updateCamp } from "../controllers/camp.js";
+import { createCamp, deleteCamp, getCamp, getCamps, updateCamp, getCampsAd } from "../controllers/camp.js";
 
 const router = express.Router();
 
@@ -12,5 +12,7 @@ router.delete("/:id", deleteCamp);
 router.get("/:id", getCamp);
 
 router.get("/", getCamps);
+
+router.get("/reg/:mobile", getCampsAd);
 
 export default router;
