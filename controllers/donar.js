7b67babaaa6e0c6b8f8b0631b4596f6458db8158
Soumya Ranjan Sahu <88780525+SaM-0777@users.noreply.github.com>
2022-11-06 18:25:24 +0530

    Update donar.js

--- controllers/donar.js
+++ controllers/donar.js
@@ -33,7 +33,16 @@ export const deleteDonar = async (req, res, next) => {
 export const getDonar = async (req, res, next) => {
     try {
         const donar = await Donar.findById(req.params.id)
-        res.status(200).json(donar);
+        res.status(200).json(donar)
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const getDonarEjs = async (req, res, next) => {
+    try {
+        const donar = await Donar.findById(req.params.id)
+        res.status(200).render('donar', { donar })
     } catch (err) {
         next(err);
     }
@@ -42,17 +51,8 @@ export const getDonar = async (req, res, next) => {
 export const getDonars = async (req, res, next) => {
     try {
         const donars = await Donar.find();
-        res.status(200).json({data: donars}); 
+        res.status(200).json(donars);
     } catch (err) {
         next(err)
-    }
-}
-export const getDonarAd = async (req,res,next)=>{
-    try{
-        const donar1 = await Donar.findOne({aadhar: req.params.reg});
-        console.log(donar1);
-        res.status(200).json(donar1);
-    }catch(err){
-        next(err);
     }
 }
