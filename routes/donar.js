874e9bb48786c40807d7f27fd14356490f6e6eae
Soumya Ranjan Sahu <88780525+SaM-0777@users.noreply.github.com>
2022-11-06 23:10:20 +0530

    Update donar.js

--- routes/donar.js
+++ routes/donar.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { createDonar, deleteDonar, getDonar, getDonars, updateDonar, getDonarEjs } from "../controllers/donar.js";
+import { createDonar, deleteDonar, getDonar, getDonars, updateDonar, getDonarEjs, updateDonarLocation } from "../controllers/donar.js";
 
 const router = express.Router();
 
 router.post("/", createDonar);
+
+router.post("/admin/:donarId", updateDonarLocation);
 
 router.put("/:id", updateDonar);
 
