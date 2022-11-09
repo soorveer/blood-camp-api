323666074b51514eb7a85f6c4262a4fd99720465
Rishav045 <82647816+Rishav045@users.noreply.github.com>
2022-11-09 19:45:47 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,25 +1,16 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
     email: {
+        unique: true,
         type: String,
         required: true
     },
     password: {
         type: String,
         required: true
-    },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
-},
-    { timestamps: true }
+    }
+    
 );
 
 export default mongoose.model("usercontents", userSchema);
